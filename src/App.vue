f9f460eebcae05fc053cf2e5a05931f38aaac7b9
Jude <jude@jude7230.local>
2022-08-03 20:32:34 +0900

    init project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h2>태그 치환하기</h2>
+    <div class="group_inp">
+      <input
+        type="text"
+        v-model="txt"
+        @keyup.enter="changeTxt"
+        class="inp_txt" />
+      <button
+        type="button"
+        class="btn"
+        @click="changeTxt">
+        변환
+      </button>
+    </div>
+
+    <div
+      class="result"
+      v-if="result">
+      {{ result }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      txt: '',
+      result: ''
+    }
+  },
+  methods: {
+    changeTxt() {
+      this.result = this.txt.replaceAll(' #', ',').replace('#', '')
+    }
+  }
+}
+</script>
